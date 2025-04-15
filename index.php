1b2685af2418082d57cf63e6994ba803b695313c
C0d3-5t3w <stewy1994@gmail.com>
2025-04-14 22:19:02 -0700

    Update

--- index.php
+++ index.php
@@ -10,11 +10,9 @@
     <link rel="stylesheet" href="/pkg/assets/css/style.css">
 </head>
 <body class="content">
-    <main>
-        <h1 class="fade-in">Welcome to TypeScript Goodies</h1>
-        <h2 class="fade-in">Explore the power of TypeScript with this basic testing website.</h2>
-        <h3 class="fade-in"><a href="pkg/pages/testing.html">testing</a></h3>
-    </main>
+    <h1 class="fade-in">Welcome to TypeScript Goodies</h1>
+    <h2 class="fade-in">Explore the power of TypeScript with this basic testing website.</h2>
+    <h3 class="fade-in"><a href="pkg/pages/testing.html">testing</a></h3>
     <script src="/pkg/assets/js/main.js" type="module"></script>
     <script src="/pkg/assets/js/build.js" type="module"></script>
 </body>
