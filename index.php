075f74cc70d2d5062aee7c497b64513ce62a1439
C0d3-5t3w <stewy1994@gmail.com>
2025-04-17 10:56:00 -0700

    Update

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>forFun</title>
-    <link rel="stylesheet" href="./pkg/static/css/style.css">
-    <link rel="stylesheet" href="./pkg/static/css/fonts.css">
+    <link rel="stylesheet" href="pkg/static/css/style.css">
+    <link rel="stylesheet" href="pkg/static/css/fonts.css">
 </head>
 <body class="retro-container">
     <header class="retro-header">
@@ -26,7 +26,6 @@
         </section>
     </main>
     <script src="pkg/static/js/build.js" type="module"></script>
-    <script src="pkg/assets/js/dropdown.js" type="module"></script>
     <footer class="retro-footer" style="text-align: center; padding: 20px 0; border-top: 2px solid var(--retro-primary); background-color: var(--retro-background-footer);">
         <p>&copy; <?php echo date("Y"); ?> forFun</p>
     </footer>
